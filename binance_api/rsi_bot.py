da039ff79e7a1fea95f30119ca6503a519048504
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-07-14 20:22:14 +0300

    .

--- binance_api/rsi_bot.py
+++ binance_api/rsi_bot.py
@@ -2,6 +2,7 @@ import websocket, json, pprint
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+closes = []
 
 def on_open(ws):
     print("open connection")
@@ -10,8 +11,22 @@ def on_close(ws):
     print("closed connection")
     
 def on_message(ws, message):  
+    global closes
+    
     json_message = json.loads(message)
-    pprint.pprint(f"message received\n{json_message}\n") 
+    pprint("\nmessage received") 
+    pprint.pprint(json_message)
+    
+    
+    candle = json_message['k']
+    
+    is_candle_closed = candle['x']
+    close = candle['c']
+    
+    if is_candle_closed:
+        print("\ncandle closed at {}\n".format(close))
+        closes.append(float(close))
+        print(f"\ncloses\n{closes}\n")
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 
