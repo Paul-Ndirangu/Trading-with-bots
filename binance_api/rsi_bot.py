5eae8879b997de9a014d2e72381daea67c77ba92
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-07-14 19:38:20 +0300

    sockets

--- binance_api/rsi_bot.py
+++ binance_api/rsi_bot.py
@@ -1,3 +1,6 @@
 import websocket
 
-ws = websocket.WebSocket
+SOCKET = "wss://stream.binance.com:9443 or wss://stream.binance.com:443/ws\
+    /ethusdt@kline_1m"
+
+ws = websocket.WebSocketApp(SOCKET)
