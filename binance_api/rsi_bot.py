5330ce79272ded4679e3252299b2196b873ecd1c
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-07-14 19:56:53 +0300

    .

--- binance_api/rsi_bot.py
+++ binance_api/rsi_bot.py
@@ -1,17 +1,18 @@
-import websocket
+import websocket, json, pprint
 
-SOCKET = "wss://stream.binance.com:9443 or wss://stream.binance.com:443/ws\
-    /ethusdt@kline_1m"
+SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
 
-def on_open():
+def on_open(ws):
     print("open connection")
     
-def on_close():
+def on_close(ws):
     print("closed connection")
     
-def on_message():
-    print("message received")   
+def on_message(ws, message):  
+    json_message = json.loads(message)
+    pprint.pprint(f"message received\n{json_message}\n") 
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
 
+ws.run_forever()
